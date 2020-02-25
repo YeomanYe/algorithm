22e0c56f7d12a3372ea389a3ee8896a2ca4d0c0e
yeming <yeming03@baidu.com>
2020-02-25 13:11:32 +0800

    U leetcode

--- leetcode/44.js
+++ leetcode/44.js
@@ -44,7 +44,7 @@ var isMatch = function(s, p) {
             if(p[j - 1] === s[i - 1] || p[j - 1] === '?'){
                 dp[i][j] = dp[i - 1][j - 1];
             } else if(p[j - 1] === '*') {
-                dp[i][j] = dp[i][j - 1] || dp[i - 1][j];
+                dp[i][j] = dp[i][j - 1] || dp[i - 1][j] || dp[i - 1][j - 1];
             }
         }
     }
