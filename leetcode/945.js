0ac264283eab875e73655f1772e834678cfc02f3
yeming <yeming03@baidu.com>
2020-03-26 21:57:29 +0800

    U leetcode

--- leetcode/945.js
+++ leetcode/945.js
@@ -3,18 +3,38 @@
  * @return {number}
  */
 var minIncrementForUnique = function(A) {
-    let set = new Set();
     let ans = 0;
-    for(let n of A) {
-        if(!set.has(n)) {
-            set.add(n);
-        } else {
-            while(set.has(n)) {
-                n++;
-                ans++;
-            }
-            set.add(n);
+    A.sort((a,b) => a - b);
+    for(let i = 1, len = A.length;i < len;i++) {
+        if(A[i] <= A[i - 1]) {
+            let d = A[i - 1] - A[i] + 1;
+            ans += d;
+            A[i] += d;
         }
+    }
+    return ans;
+};
+
+/**
+ * @param {number[]} A
+ * @return {number}
+ */
+var minIncrementForUnique = function(A) {
+    let ans = 0;
+    let pos = [];
+    let findPos = function (i) {
+        let a = pos[i];
+        if(a === undefined) {
+            pos[i] = i;
+            return i;
+        }
+        let index = findPos(a + 1);
+        pos[i] = index;
+        return index;
+    };
+    for(let n of A) {
+        let p = findPos(n);
+        ans += p - n;
     }
     return ans;
 };
